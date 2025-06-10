ea8bc4c99be6dda4806102b6196dec6093c8184d
Joshiva Yzair <joshivatapia@gmail.com>
2025-06-09 18:52:19 -0700

    add-new-work-experience

--- src/app/pages/work-experience/experience.ts
+++ src/app/pages/work-experience/experience.ts
@@ -7,4 +7,12 @@ export const experienceList = [
     dateEnd:'Jun-2024',
     description: 'Developed a RESTful API using .NET 6, JWT, and PostgreSQL, improving server efficiency by 20%. Implemented a role-based security system to ensure controlled access. Built a web application with Angular and jQuery, integrating authentication and security features for seamless communication with REST APIs and role-based access control.',
   },
+  {
+    name: 'NotiSafe - Educational Notification System',
+    Company: 'CBTIS NO.129',
+    companyLink:'http://cbtis-129.com.mx/',
+    dateStart: 'Feb-2025',
+    dateEnd:'present',
+    description: 'Development of a cross-platform school notification system focused on tracking and communicating student check-ins and check-outs. The solution includes: A web application developed with .NET 6 for managing attendance records, user administration, and report generation. A desktop application (WinForms) used by administrative staff to register physical entries from access control points. The system sends real-time automatic notifications to guardians via email, WhatsApp, and SMS, informing them of their children’s arrivals and departures. Additionally, parents or guardians can access the web system to view their childrens attendance history, detailed reports, and closely monitor their school day. The system also supports filtering by student, group, date, and type of operation.',
+  },
 ];
