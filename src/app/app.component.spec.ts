0287d401756de186b3f4b1aa0608402beec705b8
Joshiva Yzair <joshivatapia@gmail.com>
2026-03-03 13:16:12 -0700

    ,

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,6 @@ describe('AppComponent', () => {
   it(`should have the 'portafolio' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('portafolio');
   });
 
   it('should render title', () => {
